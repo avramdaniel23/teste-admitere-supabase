0863eabd32164e89b4a51e15b6665aba9389acc9
avram <daniel.avram23@gmail.com>
2024-07-05 11:14:42 +0300

    configure quizz

---
+++ app/api/get/filteredQuestions/route.ts
@@ -0,0 +1,11 @@
+import connect from "@/utils/mongodb/startMongo";
+
+export async function GET(request: Request) {
+  const client = await connect;
+  const cursor = await client
+    .db("Teste_Admitere")
+    .collection("questions")
+    .find();
+  const greetings = await cursor.toArray();
+  return Response.json(greetings);
+}
