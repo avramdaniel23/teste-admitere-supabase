d10fbcc6284f88f162e89a7803d78c5e01d47352
Bogdan Stefanescu <26150024+bog1200@users.noreply.github.com>
2024-07-09 15:11:25 +0300

    Desktop Sidebar - V2

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -40,7 +40,7 @@ export default async function DashboardLayout({ children }: any) {
   const sunrise = convertTime12to24(timeData.results.sunrise);
   const sunset = convertTime12to24(timeData.results.sunset);
   return (
-      <div className="flex-1 w-full flex flex-col items-center">
+      <div className="flex-1 w-full flex flex-col items-center lg:h-full">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-[75px] ">
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
             <div className="flex lg:hidden ">
@@ -50,19 +50,22 @@ export default async function DashboardLayout({ children }: any) {
             <DarkModeBtn sunset={sunset} sunrise={sunrise}></DarkModeBtn>
           </div>
         </nav>
-
+          <div className="w-full h-full flex">
               <aside
-                  className="h-full border-t border-t-foreground/10 hidden lg:flex z-10 justify-center text-center text-xs bg-green-500">
+                  className="border-t border-t-foreground/10 hidden lg:flex lg:w-fit sticky justify-center text-center text-xs bg-green-500 lg:h-svh">
                   <QuickNavigation/>
               </aside>
-              <main className="h-full flex-1 w-full p-4 md:p-10 lg:max-w-7xl lg:mx-auto bg-red-500 lg:ms-[10%]">
+              <main className="h-full flex-1 w-full p-4 md:p-10 lg:max-w-7xl lg:mx-auto bg-red-500">
                   {children}
               </main>
 
-          <footer
-              className="w-full h-full border-t border-t-foreground/10 flex justify-center text-center text-xs lg:hidden">
-          <QuickNavigation/>
-        </footer>
+              <footer
+                  className="w-full h-fit border-t border-t-foreground/10 flex justify-center text-center text-xs lg:hidden fixed lg:-z-10 bottom-0 lg:left-0 mt-10 lg:mt-0 lg:bottom-auto">
+                  <QuickNavigation/>
+              </footer>
+          </div>
+
+
       </div>
   );
 }
