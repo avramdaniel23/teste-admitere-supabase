762e195e24ee0e9b6a402617d67cdcf8b4551a0a
CaptainSlow22 <razvandumitru2002@yahoo.com>
2024-07-17 19:11:45 +0300

    git merge for branch update

--- components/Sections/LeaderboardSection.tsx
+++ components/Sections/LeaderboardSection.tsx
@@ -1,124 +1,69 @@
-"use client";
-import { useState, useEffect, FC } from "react";
+"use client"
+import LeaderboardComponent from "../LeaderboardComponent";
+import { useState } from "react";
 
-interface SubjectsProps {
-    subjectName: string
+export default function LeaderboardSection() {
+    const [subject,setSubject] = useState("");
+    const [showGeneral,setShowGeneral] = useState(true);
+    
+    const toggleGeneral = () => {
+      setSubject("General");
+      setShowGeneral(true);
+  }
+
+    const toggleAlgebra = () => {
+        setSubject("Algebra si Analiza");
+        setShowGeneral(false);
+    }
+
+    const toggleFizica = () => {
+        setSubject("Fizica");
+        setShowGeneral(false);
+    }
+
+    const toggleTrigo = () => {
+        setSubject("Trigonometrie");
+        setShowGeneral(false);
+    }
+
+    const toggleInfo = () => {
+      setSubject("Informatica");
+      setShowGeneral(false);
+    }
+
+    const toggleChimie = () => {
+      setSubject("Chimie");
+      setShowGeneral(false);
+    }
+
+    const toggleEconomie = () => {
+      setSubject("Economie");
+      setShowGeneral(false);
+    }
+  
+  return( 
+    <div className="lg:mx-auto">
+      <div className="l p-2 lg:p-4 overflow-x-auto lg:overflow-hidden">
+                <div className="flex justify-start space-x-2 md:justify-center lg:justify-center">
+                    <button onClick={toggleGeneral} className="px-3 py-1 text-md text-white bg-black rounded-3xl mx-1 my-1">General</button>
+                    <button onClick={toggleAlgebra} className="px-3 py-1 text-md text-white bg-black rounded-3xl mx-1 my-1 whitespace-nowrap">Algebra si analiza</button>
+                    <button onClick={toggleTrigo} className="px-3 py-1 text-md text-white bg-black rounded-3xl mx-1 my-1">Trigonometrie</button>
+                    <button onClick={toggleFizica} className="px-3 py-1 text-md text-white bg-black rounded-3xl mx-1 my-1">Fizica</button>
+                    <button onClick={toggleChimie} className="px-3 py-1 text-md text-white bg-black rounded-3xl mx-1 my-1">Chimie</button>
+                    <button onClick={toggleInfo} className="px-3 py-1 text-md text-white bg-black rounded-3xl mx-1 my-1">Informatica</button>
+                    <button onClick={toggleEconomie} className="px-3 py-1 text-md text-white bg-black rounded-3xl mx-1 my-1">Economie</button>
+                </div>
+        </div>
+
+        <div>
+          {showGeneral && (
+            <LeaderboardComponent subjectName="General"></LeaderboardComponent>
+          )}
+
+          {!showGeneral && (
+            <LeaderboardComponent subjectName={`${subject}`}></LeaderboardComponent>
+          )}
+        </div>
+    </div>
+  )
 }
-
-const students = [
-    {
-        nume: "Petracovici",
-        prenume: "Maximilian",
-        place: 1,
-        grade: 0,
-        score: 0
-    },
-    {
-        nume: "Petracovici",
-        prenume: "Maximilian",
-        place: 2,
-        grade: 0,
-        score: 0
-    },
-    {
-        nume: "Petracovici",
-        prenume: "Maximilian",
-        place: 3,
-        grade: 0,
-        score: 0
-    },
-    {
-        nume: "Petracovici",
-        prenume: "Maximilian",
-        place: 4,
-        grade: 0,
-        score: 0
-    },
-    {
-        nume: "Petracovici",
-        prenume: "Maximilian",
-        place: 5,
-        grade: 0,
-        score: 0
-    },
-    {
-        nume: "Petracovici",
-        prenume: "Maximilian",
-        place: 6,
-        grade: 0,
-        score: 0
-    },
-    {
-        nume: "Petracovici",
-        prenume: "Maximilian",
-        place: 7,
-        grade: 0,
-        score: 0
-    },
-]
-
-const LeaderboardSection: FC<SubjectsProps> = ({subjectName}) => {
-    const podium = students.slice(0,3);
-    const rest = students.slice(3,students.length)
-
-    return (
-        <section className="lg:mx-auto">
-            <div className="flex flex-col items-center justify-center mx-auto max-w-[80rem] px-[1rem] pt-[2rem]  lg:px-0 lg:pt-[3rem]">
-            <div className="mb-[100px] z-10">
-                <h3 className="text-3xl uppercase font-bold">{subjectName}</h3>
-            </div>
-            <div className="flex items-end mt-10">
-                <div className="relative bg-gradient-to-t from-stone-400 to-stone-700 h-[130px] w-[120px] border-r-2 p-4 rounded-t-2xl shadow-xl flex flex-col items-center md:w-[180px] lg:w-[160px]">
-                    <div className="absolute -top-[110px] flex flex-col items-center z-10">
-                        <img className="h-[50px] w-[50px] rounded-lg border-2 border-black" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg"></img>
-                        <span className="font-bold justify-center lg:text-xl" >{podium[1].nume}</span>
-                        <span className="font-bold justify-center lg:text-xl" >{podium[1].prenume}</span>
-                    </div>
-                    <span className="font-bold text-white justify-center text-2xl">{podium[1].place}nd</span>
-                    <div className="absolute bottom-0 left-0 right-0 flex justify-center p-2">
-                        <span className="text-white font-bold">{podium[1].score} puncte</span>
-                    </div>
-                </div>
-                <div className="relative bg-gradient-to-t from-yellow-400 to-yellow-700 h-[160px] w-[120px] border-r-2 flex flex-col p-4 rounded-t-2xl shadow-xl items-center md:w-[150px] lg:w-[150px]">
-                    <div className="absolute -top-[110px] flex flex-col items-center z-10">
-                        <img className="h-[50px] w-[50px] rounded-lg border-2 border-black" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg"></img>
-                        <span className="font-bold justify-center lg:text-xl" >{podium[0].nume}</span>
-                        <span className="font-bold justify-center lg:text-xl" >{podium[0].prenume}</span>
-                    </div>
-                    <span className="font-bold text-white justify-center text-2xl">{podium[0].place}st</span>
-                    <div className="absolute bottom-0 left-0 right-0 flex justify-center p-2">
-                        <span className="text-white font-bold">{podium[0].score} puncte</span>
-                    </div>        
-                </div>
-                <div className="relative bg-gradient-to-t from-amber-600 to-amber-800 h-[100px] w-[120px] border-r-2 p-4 flex flex-col rounded-t-2xl shadow-xl items-center md:w-[150px] lg:w-[150px]">
-                    <div className="absolute -top-[110px] flex flex-col items-center z-10">
-                        <img className="h-[50px] w-[50px] rounded-lg border-2 border-black" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg"></img>
-                        <span className="font-bold  justify-center lg:text-xl" >{podium[2].nume}</span>
-                        <span className="font-bold justify-center lg:text-xl" >{podium[2].prenume}</span>
-                    </div>
-                    <span className="font-bold text-white justify-center text-2xl">{podium[2].place}rd</span>
-                    <div className="absolute bottom-0 left-0 right-0 flex justify-center p-2">
-                        <span className="text-white font-bold ">{podium[2].score} puncte</span>
-                    </div>
-                </div>
-            </div>
-            </div>
-            <div className="flex flex-col mx-auto mt-5 bg-white rounded-xl shadow-xl lg:w-[55rem]">
-                {rest.map((student,idx) => (
-                    <div key={idx} className="flex justify-between p-4 items-center border-b-2 border-gray">
-                    <div className="flex items-center">
-                        <p className="text-xl mr-4">{student.place}</p>
-                        <img className="h-[50px] w-[50px] rounded-lg mr-4 border-2 border-black" src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg"></img>
-                        <p className="text-xl ml-2">{student.nume}</p>
-                        <p className="text-xl ml-2 hidden md:block lg:block">{student.prenume}</p>
-                    </div>
-                    <p className="text-xl p-2 hidden md:block lg:block">{student.grade}/10</p>
-                    <p className="text-xl px-3 py-1 text-white bg-black rounded-3xl whitespace-nowrap">{student.score} puncte</p>
-                </div>
-                ))}
-            </div>
-        </section>
-    )
-};
-
-export default LeaderboardSection;
