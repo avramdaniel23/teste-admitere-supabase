c524d78472e13e641b83440398112a3a53a6dcd9
Petru <slavilapetru@gmail.com>
2024-07-24 14:42:36 +0300

    resullt page styles

--- app/dashboard/quizzes/join/results/page.tsx
+++ app/dashboard/quizzes/join/results/page.tsx
@@ -117,26 +117,26 @@ export default function Results() {
     }
 
     return (
-        <div>
+        <div className="mb-20">
             {quizData && (
-                <div className="mb-5 font-medium">
+                <div className="mb-5 font-medium rounded-md shadow-grey-3 shadow-md">
                     <div className="text-center text-[28px]">Rezultatul testuloui {quizData.name}</div>
                     <p className="my-4 text-center text-[20px]">Scor: {thisSubmission.score}/{thisSubmission.submission_answers.length * 10}</p>
-
                 </div>
             )}
 
             {questionsData && (
-                <div>
+                <div className="flex flex-col gap-10">
                     {questionsData.map((question, index) => (
-                        <div key={index} className="mb-7 shadow-md rounded-lg">
-                            <p className="p-2 text-justify text-[18px] text-white bg-blue-600 rounded-t-lg">{index + 1}. {question.question}</p>
+                        <div key={index} className="flex flex-col gap-4 p-4 shadow-grey-3 shadow-md rounded-lg">
+                            <p className="p-2 text-justify">{index + 1}. {question.question}</p>
+                            <div className="h-1 w-full bg-zinc-300"></div>
                             {thisSubmission && thisSubmission.submission_answers[index] && (
                                 <div>
-                                    <div key={index} className="p-2">Răspunsul tău: <span className={`${thisSubmission.submission_answers[index].is_correct == false ? "text-red-600" : "text-black"}`}>{thisSubmission.submission_answers[index].selected_answer_id}</span></div>
+                                    <div key={index} className={`p-2 rounded-md ${thisSubmission.submission_answers[index].is_correct == false ? "bg-red-300" : "bg-green-300"}`}>Răspunsul tău: <span>{thisSubmission.submission_answers[index].selected_answer_id}</span></div>
                                 </div>
                             )}
-                            <div className="w-full p-2  rounded-b-lg bg-green-300 opacity-50"><p>Răspunsul corect este: {question.correct_answer}</p></div>
+                            <div className={` ${thisSubmission.submission_answers[index].is_correct ? "" : "text-black"} bg-zinc-200 w-full p-2 rounded-lg`}><p>Răspunsul corect este: {question.correct_answer}</p></div>
                         </div>
                     ))}
                 </div>
